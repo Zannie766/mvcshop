0c1868332819047696a7e31cbc495c887ea1063e
Zannie766 <thaituvan44@gmail.com>
2025-04-24 07:58:05 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'base' => '/PTITCNTT01/ss20/',
+    'assets' => '/PTITCNTT01/ss20/assets/',
+    'baseURL' => 'http://localhost/PTITCNTT01/ss20/', 
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'ProductDB',
+        'username' => 'root',
+        'password' => ''
+    )
+);
